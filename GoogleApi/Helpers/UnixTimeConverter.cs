8b488b68186b0c148da04c115a423d3afcc9ccbb
Michael Vivet <mv@clickataxi.com>
2013-07-23 19:01:04 +0000

    Changed Unix converter to work with UTC dates

--- GoogleApi/Helpers/UnixTimeConverter.cs
+++ GoogleApi/Helpers/UnixTimeConverter.cs
@@ -4,14 +4,14 @@ namespace GoogleApi.Helpers
 {
 	public static class UnixTimeConverter
 	{
-		private static DateTime _epoch = new DateTime(1970, 1, 1);
+		private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
 		/// <summary>
 		/// Converts a DateTime to a Unix timestamp
 		/// </summary>
 		public static int DateTimeToUnixTimestamp(DateTime _dateTime)
 		{
-			return (int)(_dateTime - _epoch.ToLocalTime()).TotalSeconds;
+			return (int)(_dateTime - _epoch).TotalSeconds;
 		}
 	}
 }
