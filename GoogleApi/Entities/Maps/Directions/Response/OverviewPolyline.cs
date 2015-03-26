b9f0951b69a8565541c8f1f6b51df0a5cbcc3352
Michael Vivet <vivet@vivetonline.com>
2015-03-26 07:31:55 +0100

    Made Directions PolyLine decode to use the GoogleFunctions.DecodePolyLine

--- GoogleApi/Entities/Maps/Directions/Response/OverviewPolyline.cs
+++ GoogleApi/Entities/Maps/Directions/Response/OverviewPolyline.cs
@@ -27,66 +27,7 @@ namespace GoogleApi.Entities.Maps.Directions.Response
 
 		public OverviewPolyline()
 		{
-			InitLazyPoints(default(StreamingContext));
-		}
-
-		[OnDeserializing]
-		private void InitLazyPoints(StreamingContext _contex)
-		{
-			_pointsLazy = new Lazy<IEnumerable<Location>>(DecodePoints);
-		}
-
-		/// <summary>
-        /// Adapted from http://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
-        /// The algorithm explained here - https://developers.google.com/maps/documentation/utilities/polylinealgorithm
-        /// </summary>
-		/// <returns></returns>
-		/// <exception cref="PointsDecodingException">Unexpectedly couldn't decode points</exception>
-		private IEnumerable<Location> DecodePoints()
-		{
-			IEnumerable<Location> _points;
-
-			try
-			{
-				var _poly = new List<Location>();
-                var _index = 0;
-				var _lat = 0;
-                var _lng = 0;
-
-                while (_index < this.EncodedPoints.Length)
-				{
-					int _b, _shift = 0, _result = 0;
-					do
-					{
-                        _b = this.EncodedPoints[_index++] - 63;
-						_result |= (_b & 0x1f) << _shift;
-						_shift += 5;
-					} while (_b >= 0x20);
-                    var _dlat = ((_result & 1) != 0 ? ~(_result >> 1) : (_result >> 1));
-					_lat += _dlat;
-
-					_shift = 0;
-					_result = 0;
-					do
-					{
-                        _b = this.EncodedPoints[_index++] - 63;
-						_result |= (_b & 0x1f) << _shift;
-						_shift += 5;
-					} while (_b >= 0x20);
-					var _dlng = ((_result & 1) != 0 ? ~(_result >> 1) : (_result >> 1));
-					_lng += _dlng;
-
-					_poly.Add(new Location((int)((_lat / 1E5) * 1E6), (int)((_lng / 1E5) * 1E6)));
-				}
-
-				_points = _poly.ToArray();
-			}
-			catch (Exception _ex)
-			{
-                throw new PointsDecodingException("Couldn't decode points", this.EncodedPoints, _ex);
-			}
-
-			return _points;
+			this.InitLazyPoints(default(StreamingContext));
 		}
 
 		/// <summary>
@@ -97,5 +38,22 @@ namespace GoogleApi.Entities.Maps.Directions.Response
 		{
 			return this.EncodedPoints;
 		}
-	}
+
+        [OnDeserializing]
+        private void InitLazyPoints(StreamingContext _contex)
+        {
+            _pointsLazy = new Lazy<IEnumerable<Location>>(DecodePoints);
+        }
+        private IEnumerable<Location> DecodePoints()
+        {
+            try
+            {
+                return GoogleFunctions.DecodePolyLine(this.EncodedPoints);
+            }
+            catch (Exception _ex)
+            {
+                throw new PointsDecodingException("Couldn't decode points", this.EncodedPoints, _ex);
+            }
+        }
+    }
 }
