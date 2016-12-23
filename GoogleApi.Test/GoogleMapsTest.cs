04b0c9a894a9f9b6cd1fd097fff191aaa5c69df1
Michael Vivet <vivet@vivetonline.com>
2016-12-23 17:39:21 +0100

    Added more test to maps and places.

--- GoogleApi.Test/GoogleMapsTest.cs
+++ GoogleApi.Test/GoogleMapsTest.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Net;
 using System.Threading;
-using System.Threading.Tasks;
 using GoogleApi.Entities.Common;
 using GoogleApi.Entities.Maps.Directions.Request;
 using GoogleApi.Entities.Maps.DistanceMatrix.Request;
@@ -21,6 +20,24 @@ namespace GoogleApi.Test
             var _request = new DirectionsRequest { Origin = "285 Bedford Ave, Brooklyn, NY, USA", Destination = "185 Broadway Ave, Manhattan, NY, USA" };
 
             var _result = GoogleMaps.Directions.Query(_request);
+            var _overviewPath = _result.Routes.First().OverviewPath;
+            var _polyline = _result.Routes.First().Legs.First().Steps.First().PolyLine;
+
+            if (_result.Status == Status.OVER_QUERY_LIMIT)
+                Assert.Inconclusive("Cannot run test since you have exceeded your Google API query limit.");
+
+            Assert.AreEqual(Status.OK, _result.Status);
+            Assert.AreEqual(133, _overviewPath.Points.Count(), 5);
+            Assert.AreEqual(5, _polyline.Points.Count());
+            Assert.AreEqual(8253, _result.Routes.First().Legs.First().Steps.Sum(_s => _s.Distance.Value), 300);
+            Assert.AreEqual(355, _result.Routes.First().Legs.First().Steps.Sum(_s => _s.Duration.Value.Seconds), 50);
+        }
+        [Test]
+        public void DirectionsTestAsyncTest()
+        {
+            var _request = new DirectionsRequest { Origin = "285 Bedford Ave, Brooklyn, NY, USA", Destination = "185 Broadway Ave, Manhattan, NY, USA" };
+
+            var _result = GoogleMaps.Directions.QueryAsync(_request).Result;
             var _overviewPath = _result.Routes.First().OverviewPath;
             var _polyline = _result.Routes.First().Legs.First().Steps.First().PolyLine;
 
@@ -63,18 +80,6 @@ namespace GoogleApi.Test
             Assert.AreEqual(-73.9613119, _result.Results.First().Geometry.Location.Longitude, 0.001);
         }
         [Test]
-        public void GeocodingWhenTimeoutTest()
-        {
-            var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA" };
-            Assert.Throws<WebException>(() => GoogleMapsTest.ThrowInnerException(() => GoogleMaps.Geocode.Query(_request, TimeSpan.FromMilliseconds(1))));
-        }
-        [Test]
-        public void GeocodingWhenInvalidClientCredentialsTest()
-        {
-            var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA", ClientId = "gme-ThisIsAUnitTest", Key = "AAECAwQFBgcICQoLDA0ODxAREhM=" };
-            Assert.Throws<WebException>(() => GoogleMapsTest.ThrowInnerException(() => GoogleMaps.Geocode.Query(_request)));
-        }
-        [Test]
         public void GeocodingAsyncTest()
         {
             var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA" };
@@ -88,28 +93,58 @@ namespace GoogleApi.Test
             Assert.AreEqual(40.7140415, _result.Results.First().Geometry.Location.Latitude, 0.001);
             Assert.AreEqual(-73.9613119, _result.Results.First().Geometry.Location.Longitude, 0.001);
         }
-		[Test]
-        public void GeocodingAsyncWhenInvalidClientCredentialsTest()
-		{
-			var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA", ClientId = "gme-ThisIsAUnitTest", Key = "AAECAwQFBgcICQoLDA0ODxAREhM=" };
-            Assert.Throws<WebException>(() => GoogleMapsTest.ThrowInnerException(() => GoogleMaps.Geocode.QueryAsync(_request).Wait()));
-		}
-		[Test]
-        public void GeocodingAsyncWhenTimeoutTest()
-		{
-            var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA" };
-            Assert.Throws<TimeoutException>(() => GoogleMapsTest.ThrowInnerException(() => GoogleMaps.Geocode.QueryAsync(_request, TimeSpan.FromMilliseconds(1)).Wait()));
-		}
         [Test]
-        public void GeocodingAsyncCancelThrowsTest()
-		{
+        public void GeocodingAsyncCancelledTest()
+        {
             var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA" };
 
-			var _cancellationTokenSource = new CancellationTokenSource();
+            var _cancellationTokenSource = new CancellationTokenSource();
             var _task = GoogleMaps.Geocode.QueryAsync(_request, _cancellationTokenSource.Token);
-			_cancellationTokenSource.Cancel();
+            _cancellationTokenSource.Cancel();
 
-            Assert.Throws<TaskCanceledException>(() => GoogleMapsTest.ThrowInnerException(_task));
+            Assert.Throws<OperationCanceledException>(() =>
+            {
+                try
+                {
+                    _task.Wait(_cancellationTokenSource.Token);
+                }
+                catch (AggregateException _ex)
+                {
+                    throw _ex.Flatten().InnerException ?? _ex;
+                }
+            });
+        }
+        [Test]
+        public void GeocodingWhenTimeoutTest()
+        {
+            var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA" };
+            Assert.Throws<WebException>(() =>
+            {
+                try
+                {
+                    GoogleMaps.Geocode.Query(_request, TimeSpan.FromMilliseconds(1));
+                }
+                catch (AggregateException _ex)
+                {
+                    throw _ex.InnerException ?? _ex;
+                }
+            });
+        }
+        [Test]
+        public void GeocodingWhenInvalidClientCredentialsTest()
+        {
+            var _request = new GeocodingRequest { Address = "285 Bedford Ave, Brooklyn, NY 11211, USA", ClientId = "gme-ThisIsAUnitTest", Key = "AAECAwQFBgcICQoLDA0ODxAREhM=" };
+            Assert.Throws<WebException>(() =>
+            {
+                try
+                {
+                    GoogleMaps.Geocode.Query(_request, TimeSpan.FromMilliseconds(1));
+                }
+                catch (AggregateException _ex)
+                {
+                    throw _ex.InnerException ?? _ex;
+                }
+            });
         }
 
         [Test]
@@ -142,6 +177,11 @@ namespace GoogleApi.Test
             //var _result = GoogleMaps.Geolocation.Query(_request);
 
             //Assert.AreEqual(Status.OK, _result.Status);
+        }
+        [Test]
+        public void GeolocationAsyncTest()
+        {
+            Assert.Inconclusive();
         }
 
         [Test]
@@ -202,7 +242,6 @@ namespace GoogleApi.Test
 
             Assert.AreEqual(8247, _response.Rows.First().Elements.First().Distance.Value, 100);
         }
-
         [Test]
         public void DistanceMatrixAsyncTest()
         {
@@ -217,28 +256,10 @@ namespace GoogleApi.Test
         {
             Assert.Inconclusive();
         }
-
-        private static void ThrowInnerException(Task _task)
+        [Test]
+        public void TimeZoneAsyncTest()
         {
-            try
-            {
-                _task.Wait();
-            }
-            catch (AggregateException _ex)
-            {
-                throw _ex.Flatten().InnerException ?? _ex;
-            }
-        }
-        private static void ThrowInnerException(Action _action)
-        {
-            try
-            {
-                _action();
-            }
-            catch (AggregateException _ex)
-            {
-                throw _ex.InnerException ?? _ex;
-            }
+            Assert.Inconclusive();
         }
 	}
 }
--- GoogleApi.Test/GooglePlacesTest.cs
+++ GoogleApi.Test/GooglePlacesTest.cs
@@ -26,16 +26,15 @@ namespace GoogleApi.Test
             };
 
             var _response = GooglePlaces.AutoComplete.Query(_request);
-            var _results = _response.Predictions.ToList();
+            var _results = _response.Predictions.ToArray();
 
             Assert.AreEqual(_results[0].Description, "Jagtvej, 2200 Denmark");
             Assert.AreEqual(_results[1].Description, "Jagtvej, 2200 Copenhagen, Denmark");
             Assert.AreEqual(_results[2].Description, "Jagtvej 2200, Lemvig, Denmark");
             Assert.AreEqual(_results[3].Description, "Jagtvej 2200, Denmark");
             Assert.AreEqual(_results[4].Description, "Jagtvej 2200, Hillerød, Denmark");
-            Assert.AreEqual(5, _results.Count);
+            Assert.AreEqual(5, _results.Length);
         }
-
         [Test]
         public void PlacesAutoCompleteAsyncTest()
         {
@@ -48,14 +47,14 @@ namespace GoogleApi.Test
             };
 
             var _response = GooglePlaces.AutoComplete.QueryAsync(_request).Result;
-            var _results = _response.Predictions.ToList();
+            var _results = _response.Predictions.ToArray();
 
             Assert.AreEqual(_results[0].Description, "Jagtvej, 2200 Denmark");
             Assert.AreEqual(_results[1].Description, "Jagtvej, 2200 Copenhagen, Denmark");
             Assert.AreEqual(_results[2].Description, "Jagtvej 2200, Lemvig, Denmark");
             Assert.AreEqual(_results[3].Description, "Jagtvej 2200, Denmark");
             Assert.AreEqual(_results[4].Description, "Jagtvej 2200, Hillerød, Denmark");
-            Assert.AreEqual(5, _results.Count);
+            Assert.AreEqual(5, _results.Length);
         }
 
         [Test]
@@ -69,14 +68,56 @@ namespace GoogleApi.Test
                 Language = "en",
             };
             var _response = GooglePlaces.QueryAutoComplete.Query(_request);
-            var _results = _response.Predictions.ToList();
+            var _results = _response.Predictions.ToArray();
 
             Assert.AreEqual(_results[0].Description, "Jagtvej 2200, Denmark");
             Assert.AreEqual(_results[1].Description, "Jagtvej 2200, Lemvig, Denmark");
             Assert.AreEqual(_results[2].Description, "Jagtvej 2200, Hillerød, Denmark");
             Assert.AreEqual(_results[3].Description, "Jagtvej 2200, Næstved, Denmark");
             Assert.AreEqual(_results[4].Description, "Jagtvej 2200, Fredensborg, Denmark");
-            Assert.AreEqual(5, _results.Count);
+            Assert.AreEqual(5, _results.Length);
+        }
+        [Test]
+        public void PlacesQueryAutoCompleteAsyncTest()
+        {
+            var _request = new PlacesQueryAutoCompleteRequest
+            {
+                Key = this._apiKey,
+                Input = "jagtvej 2200",
+                Sensor = true,
+                Language = "en",
+            };
+            var _response = GooglePlaces.QueryAutoComplete.QueryAsync(_request).Result;
+            var _results = _response.Predictions.ToArray();
+
+            Assert.AreEqual(_results[0].Description, "Jagtvej 2200, Denmark");
+            Assert.AreEqual(_results[1].Description, "Jagtvej 2200, Lemvig, Denmark");
+            Assert.AreEqual(_results[2].Description, "Jagtvej 2200, Hillerød, Denmark");
+            Assert.AreEqual(_results[3].Description, "Jagtvej 2200, Næstved, Denmark");
+            Assert.AreEqual(_results[4].Description, "Jagtvej 2200, Fredensborg, Denmark");
+            Assert.AreEqual(5, _results.Length);
+        }
+
+        [Test]
+        public void PlacesAddTest()
+        {
+            Assert.Inconclusive();
+        }
+        [Test]
+        public void PlacesAddAsyncTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void PlacesDeleteTest()
+        {
+            Assert.Inconclusive();
+        }
+        [Test]
+        public void PlacesDeleteAsyncTest()
+        {
+            Assert.Inconclusive();
         }
         
         [Test]
@@ -91,9 +132,34 @@ namespace GoogleApi.Test
             };
 
             var _response = GooglePlaces.AutoComplete.Query(_request);
-            var _results = _response.Predictions.ToList();
+            var _results = _response.Predictions.ToArray();
             var _result = _results.First();
             
+            var _request2 = new PlacesDetailsRequest
+            {
+                Key = _apiKey,
+                PlaceId = _result.PlaceId,
+                Sensor = true,
+            };
+
+            var _response2 = GooglePlaces.Details.Query(_request2);
+            Assert.AreEqual(Status.OK, _response2.Status);
+        }
+        [Test]
+        public void PlacesDetailsAsyncTest()
+        {
+            var _request = new PlacesAutoCompleteRequest
+            {
+                Key = _apiKey,
+                Input = "jagtvej 2200",
+                Sensor = true,
+                Language = "en",
+            };
+
+            var _response = GooglePlaces.AutoComplete.QueryAsync(_request).Result;
+            var _results = _response.Predictions.ToArray();
+            var _result = _results.First();
+
             var _request2 = new PlacesDetailsRequest
             {
                 Key = _apiKey,
@@ -123,6 +189,24 @@ namespace GoogleApi.Test
             Assert.IsNotNull(_response);
             Assert.AreEqual(Status.OK, _response.Status);
         }
+        [Test]
+        public void PlacesNearBySearchAsyncTest()
+        {
+            var _request = new PlacesNearBySearchRequest
+            {
+                Key = this._apiKey,
+                Location = new Location(51.491431, -3.16668),
+                Sensor = true,
+                Language = "en",
+                Radius = 500,
+                Types = new[] { SearchPlaceType.SCHOOL, SearchPlaceType.POLICE }
+            };
+
+            var _response = GooglePlaces.NearBySearch.QueryAsync(_request).Result;
+
+            Assert.IsNotNull(_response);
+            Assert.AreEqual(Status.OK, _response.Status);
+        }
         
         [Test]
         public void PlacesTextSearchTest()
@@ -136,6 +220,22 @@ namespace GoogleApi.Test
             };
 
             var _response = GooglePlaces.TextSearch.Query(_request);
+
+            Assert.IsNotNull(_response);
+            Assert.AreEqual(Status.OK, _response.Status);
+        }
+        [Test]
+        public void PlacesTextSearchAsyncTest()
+        {
+            var _request = new PlacesTextSearchRequest
+            {
+                Key = this._apiKey,
+                Sensor = true,
+                Language = "en",
+                Query = "picadelly circus"
+            };
+
+            var _response = GooglePlaces.TextSearch.QueryAsync(_request).Result;
 
             Assert.IsNotNull(_response);
             Assert.AreEqual(Status.OK, _response.Status);
@@ -159,9 +259,27 @@ namespace GoogleApi.Test
             Assert.IsNotNull(_response);
             Assert.AreEqual(Status.OK, _response.Status);
         }
+        [Test]
+        public void PlacesRadarSearchAsyncTest()
+        {
+            var _request = new PlacesRadarSearchRequest
+            {
+                Key = this._apiKey,
+                Location = new Location(55.673323, 12.527438),
+                Radius = 500,
+                Sensor = true,
+                Language = "en",
+                Keyword = "abc"
+            };
+
+            var _response = GooglePlaces.RadarSearch.QueryAsync(_request).Result;
+
+            Assert.IsNotNull(_response);
+            Assert.AreEqual(Status.OK, _response.Status);
+        }
 
         [Test]
-        public void PhotosTest()
+        public void PlacesPhotosTest()
         {
             Assert.Inconclusive();
 
@@ -203,6 +321,11 @@ namespace GoogleApi.Test
             //Assert.IsNotNull(_response3);
             //Assert.IsNotNull(_response3.Photo);
             //Assert.AreEqual(Status.OK, _response3.Status);
+        }
+        [Test]
+        public void PlacesPhotosAsyncTest()
+        {
+            Assert.Inconclusive();
         }
     }
 }
