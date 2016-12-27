4bcda2a5aa9a471931458faeafa9c665fa6cac6d
Michael Vivet <vivet@vivetonline.com>
2016-12-27 12:51:47 +0100

    Fixed Photo response contract.

--- GoogleApi/Entities/Places/Photos/Response/PlacesPhotosResponse.cs
+++ GoogleApi/Entities/Places/Photos/Response/PlacesPhotosResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Runtime.Serialization;
+using GoogleApi.Entities.Common.Interfaces;
 using GoogleApi.Entities.Places.Common;
 
 namespace GoogleApi.Entities.Places.Photos.Response
@@ -8,11 +9,27 @@ namespace GoogleApi.Entities.Places.Photos.Response
     /// Places Photos Response.
     /// </summary>
     [DataContract]
-    public class PlacesPhotosResponse : BasePlacesResponse
+    public class PlacesPhotosResponse : BasePlacesResponse, IQueryStringRequest
     {       
         /// <summary>
-        /// 
+        /// A stream containing the downloded photo.
         /// </summary>
-        public MemoryStream Photo { get; set; }        
+        public virtual MemoryStream Photo { get; set; }
+
+        /// <summary>
+        /// A string containing the downloded photo.
+        /// </summary>
+        [DataMember(Name = "photo")]
+        protected virtual byte[] PhotoStr
+        {
+            get
+            {
+                return this.Photo.ToArray();
+            }
+            set
+            {
+                this.Photo = new MemoryStream(value);
+            }
+        }
     }
 }
