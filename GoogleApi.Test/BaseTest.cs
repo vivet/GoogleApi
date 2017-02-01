3cb75fad76813f3f82547266e96b6d32f3fa7fb9
Michael Vivet <mv@drivr.com>
2017-02-02 00:50:09 +0100

    Added key from file

--- GoogleApi.Test/BaseTest.cs
+++ GoogleApi.Test/BaseTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using GoogleApi.Entities.Common;
 using GoogleApi.Entities.Common.Interfaces;
 using NUnit.Framework;
@@ -7,7 +10,28 @@ namespace GoogleApi.Test
     [TestFixture]
     public abstract class BaseTest
     {
-        public string apiKey = ""; // your API key...
+        public string apiKey; // your API key...
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            try
+            {
+                const string FILENAME = "keyfile.txt";
+
+                var directoryInfo = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent?.Parent?.Parent;
+                var fileInfo = directoryInfo?.GetFiles().FirstOrDefault(x => x.Name == FILENAME);
+
+                using (var streamReader = new StreamReader(fileInfo?.FullName ?? FILENAME))
+                {
+                    this.apiKey = streamReader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                this.apiKey = string.Empty;
+            }
+        }
 
         // Nested classes
         public class TestRequest : BaseRequest
