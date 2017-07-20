2f357b4d45198c0f664a47fb9df98cd021a5b470
Michael Vivet <mv@drivr.com>
2017-07-20 18:48:41 +0200

    Added Portable (Xamarin.Forms) project

---
+++ GoogleApi.Xamarin.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GoogleApi")]
+[assembly: AssemblyDescription("Google Places, Maps, Roads, Search and Translate.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Google Api")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
