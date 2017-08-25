8e47dc94821961893037addc41be7c773e04b5e2
Michael Vivet <vivet@vivetonline.com>
2017-08-25 16:16:58 +0200

    Added Tests for Web and Image Search.

--- GoogleApi.Test/Search/Image/ImageSearchTests.cs
+++ GoogleApi.Test/Search/Image/ImageSearchTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using GoogleApi.Entities.Common.Enums;
 using GoogleApi.Entities.Search.Image.Request;
 using NUnit.Framework;
 
@@ -10,23 +14,149 @@ namespace GoogleApi.Test.Search.Image
         [Test]
         public void ImageSearchTest()
         {
-            Assert.Inconclusive();
+            var request = new ImageSearchRequest
+            {
+                Key = this.ApiKey,
+                SearchEngineId = this.SearchEngineId,
+                Query = "google"
+            };
+
+            var response = GoogleSearch.ImageSearch.Query(request);
+
+            Console.WriteLine(response.RawJson);
+
+            Assert.IsNotNull(response);
+            Assert.IsNotEmpty(response.Items);
+            Assert.AreEqual(response.Kind, "customsearch#search");
+            Assert.AreEqual(response.Status, Status.Ok);
+
+            Assert.IsNotNull(response.Url);
+            Assert.AreEqual(response.Url.Type, "application/json");
+            Assert.AreEqual(response.Url.Template, "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json");
+
+            Assert.IsNotNull(response.SearchInformation);
+            Assert.Greater(response.SearchInformation.SearchTime, 0.00);
+            Assert.IsNotEmpty(response.SearchInformation.SearchTimeFormatted);
+            Assert.Greater(response.SearchInformation.TotalResults, 0);
+            Assert.IsNotEmpty(response.SearchInformation.TotalResultsFormatted);
+
+            var context = response.Context;
+            Assert.IsNotNull(context);
+            Assert.AreEqual(context.Title, "Google Web");
+
+            var items = response.Items;
+            Assert.IsNotNull(items);
+
+            var item = response.Items.FirstOrDefault();
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Link, "https://madeby.google.com/static/images/google_g_logo.svg");
+            Assert.AreEqual(item.Title, "Google Home – Made by Google");
+            Assert.AreEqual(item.DisplayLink, "madeby.google.com");
         }
+
+        [Test]
+        public void ImageSearchAsyncTest()
+        {
+            var request = new ImageSearchRequest
+            {
+                Key = this.ApiKey,
+                SearchEngineId = this.SearchEngineId,
+                Query = "google"
+            };
+
+            var response = GoogleSearch.ImageSearch.QueryAsync(request).Result;
+
+            Assert.IsNotNull(response);
+            Assert.IsNotEmpty(response.Items);
+            Assert.AreEqual(response.Kind, "customsearch#search");
+            Assert.AreEqual(response.Status, Status.Ok);
+
+            Assert.IsNotNull(response.Url);
+            Assert.AreEqual(response.Url.Type, "application/json");
+            Assert.AreEqual(response.Url.Template, "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json");
+
+            Assert.IsNotNull(response.SearchInformation);
+            Assert.Greater(response.SearchInformation.SearchTime, 0.00);
+            Assert.IsNotEmpty(response.SearchInformation.SearchTimeFormatted);
+            Assert.Greater(response.SearchInformation.TotalResults, 0);
+            Assert.IsNotEmpty(response.SearchInformation.TotalResultsFormatted);
+
+            var context = response.Context;
+            Assert.IsNotNull(context);
+            Assert.AreEqual(context.Title, "Google Web");
+
+            var items = response.Items;
+            Assert.IsNotNull(items);
+
+            var item = response.Items.FirstOrDefault();
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Link, "https://madeby.google.com/static/images/google_g_logo.svg");
+            Assert.AreEqual(item.Title, "Google Home – Made by Google");
+            Assert.AreEqual(item.DisplayLink, "madeby.google.com");
+        }
+
+        [Test]
+        public void ImageSearchWhenAsyncAndTimeoutTest()
+        {
+            var request = new ImageSearchRequest
+            {
+                Key = this.ApiKey,
+                SearchEngineId = this.SearchEngineId,
+                Query = "google"
+            };
+
+            var exception = Assert.Throws<AggregateException>(() =>
+            {
+                var result = GoogleSearch.ImageSearch.QueryAsync(request, TimeSpan.FromMilliseconds(1)).Result;
+                Assert.IsNull(result);
+            });
+
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exception.Message, "One or more errors occurred.");
+
+            var innerException = exception.InnerException;
+            Assert.IsNotNull(innerException);
+            Assert.AreEqual(innerException.GetType(), typeof(TaskCanceledException));
+            Assert.AreEqual(innerException.Message, "A task was canceled.");
+        }
+
+        [Test]
+        public void ImageSearchWhenAsyncAndCancelledTest()
+        {
+            var request = new ImageSearchRequest
+            {
+                Key = this.ApiKey,
+                SearchEngineId = this.SearchEngineId,
+                Query = "google"
+            };
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            var task = GoogleSearch.ImageSearch.QueryAsync(request, cancellationTokenSource.Token);
+            cancellationTokenSource.Cancel();
+
+            var exception = Assert.Throws<OperationCanceledException>(() => task.Wait(cancellationTokenSource.Token));
+            Assert.IsNotNull(exception);
+            Assert.AreEqual(exception.Message, "The operation was canceled.");
+        }
+
         [Test]
         public void ImageSearchWhenImageTypeTest()
         {
             Assert.Inconclusive();
         }
+
         [Test]
         public void ImageSearchWhenImageSizeTest()
         {
             Assert.Inconclusive();
         }
+
         [Test]
         public void ImageSearchWhenImageColorTypeTest()
         {
             Assert.Inconclusive();
         }
+
         [Test]
         public void ImageSearchWhenImageDominantColorTest()
         {
@@ -38,45 +168,38 @@ namespace GoogleApi.Test.Search.Image
         {
             var request = new ImageSearchRequest
             {
-                Key = null,
-                SearchEngineId = "abc",
-                Query = "google"
+                Key = null
             };
 
             var exception = Assert.Throws<ArgumentException>(() => GoogleSearch.ImageSearch.Query(request));
-            Assert.AreEqual(exception.Message, "Key is required.");
+            Assert.AreEqual(exception.Message, "Key is required");
         }
-        [Test]
-        public void ImageSearchWhenSearchEngineIdIsNullTest()
-        {
-            var request = new ImageSearchRequest
-            {
-                Key = this.ApiKey,
-                SearchEngineId = null,
-                Query = "google"
-            };
 
-            var exception = Assert.Throws<ArgumentException>(() => GoogleSearch.ImageSearch.Query(request));
-            Assert.AreEqual(exception.Message, "SearchEngineId is required.");
-        }
         [Test]
         public void ImageSearchWhenQueryIsNullTest()
         {
             var request = new ImageSearchRequest
             {
                 Key = this.ApiKey,
-                SearchEngineId = "abc",
                 Query = null
             };
 
             var exception = Assert.Throws<ArgumentException>(() => GoogleSearch.ImageSearch.Query(request));
-            Assert.AreEqual(exception.Message, "Query is required.");
+            Assert.AreEqual(exception.Message, "Query is required");
         }
 
         [Test]
-        public void ImageSearchAsyncTest()
+        public void ImageSearchWhenSearchEngineIdIsNullTest()
         {
-            Assert.Inconclusive();
+            var request = new ImageSearchRequest
+            {
+                Key = this.ApiKey,
+                Query = "google",
+                SearchEngineId = null
+            };
+
+            var exception = Assert.Throws<ArgumentException>(() => GoogleSearch.ImageSearch.Query(request));
+            Assert.AreEqual(exception.Message, "SearchEngineId is required");
         }
     }
 }
