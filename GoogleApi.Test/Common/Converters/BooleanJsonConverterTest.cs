d55dca85d52351a6f8c824b8d1a8c08361693673
Michael Vivet <vivet@vivetonline.com>
2017-08-31 15:15:41 +0200

    Added Tests.

---
+++ GoogleApi.Test/Common/Converters/BooleanJsonConverterTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+
+namespace GoogleApi.Test.Common.Converters
+{
+    [TestFixture]
+    public class BooleanJsonConverterTest
+    {
+        [Test]
+        public void CanConvertTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void ReadJsonTest()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void WriteJsonTest()
+        {
+            Assert.Inconclusive();
+
+        }
+    }
+}
---
+++ GoogleApi.Test/Common/Extensions/StringExtensionTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace GoogleApi.Test.Common.Extensions
+{
+    [TestFixture]
+    public class StringExtensionTest
+    {
+        [Test]
+        public void ToStringEnumTest()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
