953c2410a7ca7ed38ff31accd8f3d8354fd34284
vivet <vivet@vivetonline.com>
2017-11-18 08:48:47 +0100

    Updated assembly information and file versions

--- GoogleApi.Test/Properties/AssemblyInfo.cs
+++ GoogleApi.Test/Properties/AssemblyInfo.cs
@@ -1,34 +1,33 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GoogleApi")]
-[assembly: AssemblyDescription("Google Places, Maps, Roads, Search and Translate.")]
+[assembly: AssemblyTitle("GoogleApi.Test")]
+[assembly: AssemblyDescription("GoogleApi.Test")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Google Api")]
+[assembly: AssemblyProduct("GoogleApi.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6588c2cd-d597-4a70-b84b-6e1709d224ac")]
+[assembly: Guid("ee992f73-03d1-473e-bc1a-6d3a50c3bfc1")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("3.2.8.0")]
 [assembly: AssemblyFileVersion("3.2.8.0")]
