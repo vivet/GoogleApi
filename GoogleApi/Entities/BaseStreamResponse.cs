8484ebc769ae1a8a5ace0636088fd284915147c9
vivet <vivet@vivetonline.com>
2018-03-30 21:46:38 +0200

    Added Streetview request and response

---
+++ GoogleApi/Entities/BaseStreamResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using GoogleApi.Entities.Places.Photos.Response;
+using Newtonsoft.Json;
+
+namespace GoogleApi.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BaseStreamResponse : BaseResponse
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("")]
+        public virtual byte[] Buffer { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="MemoryStream"/> based on the <see cref="PlacesPhotosResponse.PhotoBuffer"/> downloded.
+        /// </summary>
+        [JsonProperty("")]
+        public virtual MemoryStream Photo => new MemoryStream(this.Buffer ?? new byte[0]);
+    }
+}
---
+++ GoogleApi/Entities/Maps/Common/MapSize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GoogleApi.Entities.Maps.Common
+{
+	/// <summary>
+	/// Map Size.
+	/// </summary>
+	public class MapSize
+	{
+		private const int MIN_WITDTH = 1;
+		private const int MAX_WITDTH = 4096;
+		private const int MIN_HEIGHT = 1;
+		private const int MAX_HEIGHT = 4096;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="width">The width.</param>
+		/// <param name="height">The height.</param>
+		public MapSize(int width, int height)
+		{
+			if (width < 1 || width > 4096)
+				throw new ArgumentOutOfRangeException($"Width must be greater than {MapSize.MIN_WITDTH} and less than {MapSize.MAX_WITDTH}.");
+
+			if (height < 1 || height > 4096)
+				throw new ArgumentOutOfRangeException($"Width must be greater than {MapSize.MIN_HEIGHT} and less than {MapSize.MAX_HEIGHT}.");
+
+			this.Width = width;
+			this.Height = height;
+		}
+
+		/// <summary>
+		/// Width.
+		/// </summary>
+		public int Width { get; set; }
+
+		/// <summary>
+		/// Height.
+		/// </summary>
+		public int Height { get; set; }
+	}
+}
