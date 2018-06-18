a20d2757d2c267294399625478d8ef97d288c6f7
Michael Vivet <vivet@vivetonline.com>
2018-06-18 12:56:46 +0200

    Updated AssemblyInfo.cs

--- GoogleApi.net45/Properties/AssemblyInfo.cs
+++ GoogleApi.net45/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Google Api")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
