8e107a780023d52a6a63eb99ec182fffa936f0d2
Michael Vivet <vivet@vivetonline.com>
2018-08-28 18:03:33 +0200

    Added PermanentlyClosed to Places find response

--- GoogleApi/Entities/Places/Search/Find/Response/Candidate.cs
+++ GoogleApi/Entities/Places/Search/Find/Response/Candidate.cs
@@ -82,6 +82,13 @@ namespace GoogleApi.Entities.Places.Search.Find.Response
         public virtual double Rating { get; set; }
 
         /// <summary>
+        /// PermanentlyClosed is a boolean flag indicating whether the place has permanently shut down (value true). 
+        /// If the place is not permanently closed, the flag is absent from the response.
+        /// </summary>
+        [JsonProperty("permanently_closed")]
+        public virtual bool PermanentlyClosed { get; set; }
+
+        /// <summary>
         /// Photos.
         /// An array of photo objects, each containing a reference to an image. A Place Details request may return up to ten photos.
         /// </summary>
