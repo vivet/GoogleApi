7dd04899f362fe9821303dc5ba5d3928f66a0591
Michael Vivet <vivet@vivetonline.com>
2018-12-19 08:10:14 +0100

    Cosmetics

--- GoogleApi/Entities/Maps/Roads/BaseRoadsResponse.cs
+++ GoogleApi/Entities/Maps/Roads/BaseRoadsResponse.cs
@@ -10,6 +10,12 @@ namespace GoogleApi.Entities.Maps.Roads
     public abstract class BaseRoadsResponse : BaseResponse
     {
         /// <summary>
+        /// A warning message about the result quality, typically populated if the input points were too sparse
+        /// </summary>
+        [JsonProperty("warningMessage")]
+        public virtual string WarningMessage { get; set; }
+
+        /// <summary>
         /// An array of snapped points.
         /// </summary>
         [JsonProperty("snappedPoints")]
@@ -20,11 +26,5 @@ namespace GoogleApi.Entities.Maps.Roads
         /// </summary>
         [JsonProperty("errors")]
         public virtual IEnumerable<Error> Errors { get; set; }
-
-        /// <summary>
-        /// A warning message about the result quality, typically populated if the input points were too sparse
-        /// </summary>
-        [JsonProperty("warningMessage")]
-        public virtual string WarningMessage { get; set; }
     }
 }
