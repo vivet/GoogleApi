00d9ebff5ce4ba0fbd577917e0ee6825c3d9d671
vivet <vivet@vivetonline.com>
2020-09-29 08:19:01 +0200

    Fixed wrong mapping of places OpeningHours.OpenNow.

--- GoogleApi/Entities/Places/Search/Common/OpeningHours.cs
+++ GoogleApi/Entities/Places/Search/Common/OpeningHours.cs
@@ -10,7 +10,7 @@ namespace GoogleApi.Entities.Places.Search.Common
         /// <summary>
         /// OpenNow is a boolean value indicating if the place is open at the current time.
         /// </summary>
-        [JsonProperty("OpenNow")]
+        [JsonProperty("open_now")]
         public virtual bool OpenNow { get; set; }
     }
 }
