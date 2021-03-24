fb3cf154aadc55c41e5af07b5318043397044fa6
vivet <vivet@vivetonline.com>
2021-03-24 06:51:14 +0100

    Updated common language with additional languages added by Google

--- GoogleApi/Entities/Common/Enums/Language.cs
+++ GoogleApi/Entities/Common/Enums/Language.cs
@@ -9,25 +9,95 @@ namespace GoogleApi.Entities.Common.Enums
     public enum Language
     {
         /// <summary>
+        /// Afrikaans
+        /// </summary>
+        Afrikaans,
+
+        /// <summary>
+        /// Albanian
+        /// </summary>
+        Albanian,
+
+        /// <summary>
+        /// Amharic
+        /// </summary>
+        Amharic,
+
+        /// <summary>
         /// Arabic
         /// </summary>
         Arabic,
 
         /// <summary>
-        /// Bulgarian
+        /// Armenian
         /// </summary>
-        Bulgarian,
-        
+        Armenian,
+
+        /// <summary>
+        /// Azerbaijani
+        /// </summary>
+        Azerbaijani,
+
+        /// <summary>
+        /// Basque
+        /// </summary>
+        Basque,
+
+        /// <summary>
+        /// Belarusian
+        /// </summary>
+        Belarusian,
+
         /// <summary>
         /// Bengali
         /// </summary>
         Bengali,
-        
+
+        /// <summary>
+        /// Bosnian
+        /// </summary>
+        Bosnian,
+
+        /// <summary>
+        /// Bulgarian
+        /// </summary>
+        Bulgarian,
+
+        /// <summary>
+        /// Burmese
+        /// </summary>
+        Burmese,
+
         /// <summary>
         /// Catalan
         /// </summary>
         Catalan,
-        
+
+        /// <summary>
+        /// Chinese
+        /// </summary>
+        Chinese,
+
+        /// <summary>
+        /// Chinese Simplified
+        /// </summary>
+        ChineseSimplified,
+
+        /// <summary>
+        /// Chinese Hong Kong
+        /// </summary>
+        ChineseHongKong,
+
+        /// <summary>
+        /// Chinese Traditional
+        /// </summary>
+        ChineseTraditional,
+
+        /// <summary>
+        /// Croatian
+        /// </summary>
+        Croatian,
+
         /// <summary>
         /// Czech
         /// </summary>
@@ -37,9 +107,69 @@ namespace GoogleApi.Entities.Common.Enums
         /// Danish
         /// </summary>
         Danish,
-        
+
         /// <summary>
         /// Dutch
+        /// </summary>
+        Dutch,
+
+        /// <summary>
+        /// English
+        /// </summary>
+        English,
+
+        /// <summary>
+        /// English (Australian)
+        /// </summary>
+        EnglishAustralian,
+
+        /// <summary>
+        /// English (Great Britain)
+        /// </summary>
+        EnglishGreatBritain,
+
+        /// <summary>
+        /// Estonian
+        /// </summary>
+        Estonian,
+
+        /// <summary>
+        /// Farsi
+        /// </summary>
+        Farsi,
+
+        /// <summary>
+        /// Finnish
+        /// </summary>
+        Finnish,
+
+        /// <summary>
+        /// Filipino
+        /// </summary>
+        Filipino,
+
+        /// <summary>
+        /// French
+        /// </summary>
+        French,
+
+        /// <summary>
+        /// French Canada
+        /// </summary>
+        FrenchCanada,
+
+        /// <summary>
+        /// Galician
+        /// </summary>
+        Galician,
+
+        /// <summary>
+        /// Georgian
+        /// </summary>
+        Georgian,
+
+        /// <summary>
+        /// German
         /// </summary>
         German,
         
@@ -47,77 +177,32 @@ namespace GoogleApi.Entities.Common.Enums
         /// Greek
         /// </summary>
         Greek,
-        
-        /// <summary>
-        /// English
-        /// </summary>
-        English,
-        
-        /// <summary>
-        /// English (Australian)
-        /// </summary>
-        EnglishAustralian,
-        
-        /// <summary>
-        /// English (Great Britain)
-        /// </summary>
-        EnglishGreatBritain,
-        
-        /// <summary>
-        /// Spanish
-        /// </summary>
-        Spanish,
-        
-        /// <summary>
-        /// Basque
-        /// </summary>
-        Basque,
-        
-        /// <summary>
-        /// Farsi
-        /// </summary>
-        Farsi,
-        
-        /// <summary>
-        /// Finnish
-        /// </summary>
-        Finnish,
-        
-        /// <summary>
-        /// Filipino
-        /// </summary>
-        Filipino,
-        
-        /// <summary>
-        /// French
-        /// </summary>
-        French,
-        
-        /// <summary>
-        /// Galician
-        /// </summary>
-        Galician,
-        
+
         /// <summary>
         /// Gujarati
         /// </summary>
         Gujarati,
-        
+
+        /// <summary>
+        /// Hebrew
+        /// </summary>
+        Hebrew,
+
         /// <summary>
         /// Hindi
         /// </summary>
         Hindi,
-        
-        /// <summary>
-        /// Croatian
-        /// </summary>
-        Croatian,
-        
+
         /// <summary>
         /// Hungarian
         /// </summary>
         Hungarian,
-        
+
+        /// <summary>
+        /// Icelandic
+        /// </summary>
+        Icelandic,
+
         /// <summary>
         /// Indonesian
         /// </summary>
@@ -127,12 +212,7 @@ namespace GoogleApi.Entities.Common.Enums
         /// Italian
         /// </summary>
         Italian,
-        
-        /// <summary>
-        /// Hebrew
-        /// </summary>
-        Hebrew,
-        
+
         /// <summary>
         /// Japanese
         /// </summary>
@@ -142,22 +222,52 @@ namespace GoogleApi.Entities.Common.Enums
         /// Kannada
         /// </summary>
         Kannada,
-        
+
+        /// <summary>
+        /// Kazakh
+        /// </summary>
+        Kazakh,
+
+        /// <summary>
+        /// Khmer
+        /// </summary>
+        Khmer,
+
         /// <summary>
         /// Korean
         /// </summary>
         Korean,
-        
+
         /// <summary>
-        /// Lithuanian
+        /// Kyrgyz
         /// </summary>
-        Lithuanian,
-        
+        Kyrgyz,
+
+        /// <summary>
+        /// Lao
+        /// </summary>
+        Lao,
+
         /// <summary>
         /// Latvian
         /// </summary>
         Latvian,
-        
+
+        /// <summary>
+        /// Lithuanian
+        /// </summary>
+        Lithuanian,
+
+        /// <summary>
+        /// Macedonian
+        /// </summary>
+        Macedonian,
+
+        /// <summary>
+        /// Malay
+        /// </summary>
+        Malay,
+
         /// <summary>
         /// Malayalam
         /// </summary>
@@ -167,12 +277,17 @@ namespace GoogleApi.Entities.Common.Enums
         /// Marathi
         /// </summary>
         Marathi,
-        
+
         /// <summary>
-        /// Dutch
+        /// Mongolian
         /// </summary>
-        Dutch,
-        
+        Mongolian,
+
+        /// <summary>
+        /// Nepali
+        /// </summary>
+        Nepali,
+
         /// <summary>
         /// Norwegian
         /// </summary>
@@ -197,7 +312,12 @@ namespace GoogleApi.Entities.Common.Enums
         /// Portuguese (Portugal)
         /// </summary>
         PortuguesePortugal,
-        
+
+        /// <summary>
+        /// Punjabi
+        /// </summary>
+        Punjabi,
+
         /// <summary>
         /// Romanian
         /// </summary>
@@ -207,7 +327,17 @@ namespace GoogleApi.Entities.Common.Enums
         /// Russian
         /// </summary>
         Russian,
-        
+
+        /// <summary>
+        /// Serbian
+        /// </summary>
+        Serbian,
+
+        /// <summary>
+        /// Sinhalese
+        /// </summary>
+        Sinhalese,
+
         /// <summary>
         /// Slovak
         /// </summary>
@@ -217,12 +347,22 @@ namespace GoogleApi.Entities.Common.Enums
         /// Slovenian
         /// </summary>
         Slovenian,
-        
+
         /// <summary>
-        /// Serbian
+        /// Spanish
         /// </summary>
-        Serbian,
-        
+        Spanish,
+
+        /// <summary>
+        /// Spanish Latin America
+        /// </summary>
+        SpanishLatinAmerica,
+
+        /// <summary>
+        /// Swahili
+        /// </summary>
+        Swahili,
+
         /// <summary>
         /// Swedish
         /// </summary>
@@ -242,12 +382,7 @@ namespace GoogleApi.Entities.Common.Enums
         /// Thai
         /// </summary>
         Thai,
-        
-        /// <summary>
-        /// Tagalog
-        /// </summary>
-        Tagalog,
-        
+
         /// <summary>
         /// Turkish
         /// </summary>
@@ -257,20 +392,25 @@ namespace GoogleApi.Entities.Common.Enums
         /// Ukrainian
         /// </summary>
         Ukrainian,
-        
+
+        /// <summary>
+        /// Urdu
+        /// </summary>
+        Urdu,
+
+        /// <summary>
+        /// Uzbek
+        /// </summary>
+        Uzbek,
+
         /// <summary>
         /// Vietnamese
         /// </summary>
         Vietnamese,
-        
+
         /// <summary>
-        /// Chinese (Simplified)
+        /// Zulu
         /// </summary>
-        ChineseSimplified,
-        
-        /// <summary>
-        /// Chinese (Traditional)
-        /// </summary>
-        ChineseTraditional
+        Zulu
     }
 }
