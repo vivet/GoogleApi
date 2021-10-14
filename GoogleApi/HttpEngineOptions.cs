4b3872319bda428380c2e8bc27432a7a4d6022df
Michael Vivet <miv@parseport.com>
2021-10-14 07:34:13 +0200

    Added option to not throw exception on invalid request.

---
+++ GoogleApi/HttpEngineOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace GoogleApi
+{
+    /// <summary>
+    /// Http Engine Options.
+    /// </summary>
+    public class HttpEngineOptions
+    {
+        /// <summary>
+        /// Throw On Bad Request.
+        /// </summary>
+        public virtual bool ThrowOnInvalidRequest { get; set; } = true;
+    }
+}
