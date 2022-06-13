c290169605083e43cea682c3d153ff002b279782
Michael Vivet <miv@parseport.com>
2022-06-13 11:23:17 +0200

    Removed unused AppSettings in test. Updated change log

--- .tests/GoogleApi.Test/AppSettings.cs
+++ .tests/GoogleApi.Test/AppSettings.cs
@@ -7,12 +7,6 @@ public class AppSettings
     [JsonProperty("ApiKey")]
     public string ApiKey { get; set; }
 
-    [JsonProperty("CryptoKey")]
-    public string CryptoKey { get; set; }
-
-    [JsonProperty("ClientId")]
-    public string ClientId { get; set; }
-
     [JsonProperty("SearchEngineId")]
     public string SearchEngineId { get; set; }
 }
