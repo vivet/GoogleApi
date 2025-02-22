ebe3cf7e09ea27fca1418b96e560d54a39cb907f
JendaPerl <Jenda@Krynicky.cz>
2025-02-22 17:47:09 +0100

    a property specifically for the referer

--- GoogleApi/HttpEngineOptions.cs
+++ GoogleApi/HttpEngineOptions.cs
@@ -16,4 +16,20 @@ public class HttpEngineOptions {
 	/// </summary>
 	/// <example>new HttpEngineOptions { AdditionalHeaders = new() { { "Referer", "https://www.yoursite.com" } } }</example>
 	public Dictionary<string, string> AdditionalHeaders { get; set; }
+
+	/// <summary>
+	/// Specify the referer to send in the request in case your API key is restricted to specific domains.
+	/// You need to specify one of the allowed domains, if you receive "PermissionDenied: Requests from referer &lt;empty> are blocked." errors.
+	/// https://cloud.google.com/api-keys/docs/add-restrictions-api-keys#add-client-restrictions
+	/// </summary>
+	public string Referer
+	{
+		get {
+			return AdditionalHeaders != null && AdditionalHeaders.TryGetValue("Referer", out var referer) ? referer : null;
+		}
+		set {
+			AdditionalHeaders ??= [];
+			AdditionalHeaders["Referer"] = value;
+		}
+	}
 }
